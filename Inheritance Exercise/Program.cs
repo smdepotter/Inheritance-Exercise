881058fbcd1c7ba1c13dfd31e1c7f4605253ae55
Sean DePotter <seandep@live.com>
2019-06-19 03:24:06 -0400

    Add project files.

---
+++ Inheritance Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Inheritance_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            stack.Clear();
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ Inheritance Exercise/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inheritance_Exercise
+{
+    public class Stack
+    {
+        private readonly List<object> _list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("You can't add nothing.");
+
+            _list.Add(obj);
+        }
+
+        public object Pop()
+        {
+            var index = _list.Count - 1;
+
+            if (_list.Count == 0)
+                throw new InvalidOperationException("You can't pop an empty stack.");
+
+            var obj = _list[index];
+            _list.RemoveAt(index);
+            return obj;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
